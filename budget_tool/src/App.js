5238963da1337c735db26a1effe29cb3f9c440d0
John Won <johnjywon@gmail.com>
2021-07-28 15:36:47 -0400

    added routes to app

--- budget_tool/src/App.js
+++ budget_tool/src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import BucketEdit from "./Components/BucketEdit";
+import BudgetOverview from "./Components/BudgetOverview";
+import TransactionHistory from "./Components/TransactionHistory";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <BudgetOverview />
+          </Route>
+          <Route path="/bucketEdit">
+            <BucketEdit />
+          </Route>
+          <Route path="/transactionHistory">
+            <TransactionHistory />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
