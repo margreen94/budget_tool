5178c26559a8daf45c661b8ecefc1e4169a54e54
John Won <johnjywon@gmail.com>
2021-07-28 16:33:27 -0400

    Nav active works proper

--- budget_tool/src/Components/Header.js
+++ budget_tool/src/Components/Header.js
@@ -9,16 +9,16 @@ export default function Header() {
       <Navbar collapseOnSelect expand="lg" variant="dark">
         <Container>
           <LinkContainer to="/" style={{ fontSize: 28 }}>
-            <Navbar.Brand>Bugdet Tool</Navbar.Brand>
+            <Navbar.Brand>Budget Tool</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse
             className="justify-content-end"
             id="responsive-navbar-nav"
           >
-            <Nav style={{ fontSize: 20 }}>
+            <Nav className="mr-auto" style={{ fontSize: 20 }}>
               <LinkContainer to="/">
-                <Nav.Link>Budget Overview</Nav.Link>
+                <Nav.Link href="/">Budget Overview</Nav.Link>
               </LinkContainer>
               <LinkContainer to="/BucketEdit">
                 <Nav.Link>Bucket Edit</Nav.Link>
