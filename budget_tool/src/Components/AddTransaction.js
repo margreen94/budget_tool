ccecf9f9ae5285bae56111ee0ac3e2fbe59e981b
DZQ205@ALLYIPOC.COM <DZQ205@ALLYIPOC.COM>
2021-07-29 16:21:16 -0400

    added boiler plate code

---
+++ budget_tool/src/Components/AddTransaction.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export default function AddTransaction() {
+  console.log("worthless");
+
+  return (
+    <div>
+      <Container>
+        <h1>Add a Transaction</h1>
+      </Container>
+    </div>
+  );
+}
