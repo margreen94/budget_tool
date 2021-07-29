594a6a4ee0af479ffd2e8f248a84af74e3f748ee
DZQ205@ALLYIPOC.COM <DZQ205@ALLYIPOC.COM>
2021-07-29 11:08:44 -0400

    cors sucks

--- budget_tool/src/Components/BucketEdit.js
+++ budget_tool/src/Components/BucketEdit.js
@@ -1,9 +1,30 @@
 import React from "react";
+import axios from "axios";
+
+function getPercentage(e){
+  e.preventDefault()
+  axios({
+    url: "localhost:8080/getByAccountId/25",
+    method: "GET",
+  }).then((response) => {
+    console.log(response)
+  }).catch(function (error) {
+        console.log(error);  
+      })
+
+  }
 
 export default function BucketEdit() {
+
+  
+
   return (
     <div>
       <h1>Bucket </h1>
+      <form>
+        <label>Car Percentage:<input type='text'></input></label>
+        <input type='submit' onClick={getPercentage}></input>
+      </form>
     </div>
   );
 }
