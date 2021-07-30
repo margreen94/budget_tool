826cfd37fbd32bbbd9ad93c29498547c606c60ed
DZQ205@ALLYIPOC.COM <DZQ205@ALLYIPOC.COM>
2021-07-30 15:18:39 -0400

    add tansaction page

--- budget_tool/src/Components/AddTransaction.js
+++ budget_tool/src/Components/AddTransaction.js
@@ -1,57 +1,86 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import 'bootstrap/js/dist/dropdown';
+import axios from 'axios'
+import { Component } from "react";
 
 
-export default function AddTransaction() {
-  console.log("worthless");
+export default class AddTransaction extends Component {
 
-  return (
-    <div>
-      <Container>
-        <form>
-        <h3>Type:</h3>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                Transaction Type
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><a class="dropdown-item" href="#">Expense</a></li>
-                <li><a class="dropdown-item" href="#">Income</a></li>
-            </ul>
-        </div>
+    constructor(props){
+        super(props)
+        this.state = {date:'',vendor:'',tag:'',amount:''}
+        this.handleDateChange = this.handleDateChange.bind(this)
+        this.handleVendorChange = this.handleVendorChange.bind(this)
+        this.handleTagChange = this.handleTagChange.bind(this)
+        this.handleAmountChange = this.handleAmountChange.bind(this)
+        this.postToApi = this.postToApi.bind(this)
+    }
+    handleDateChange(e){
+        this.setState({date:e.target.value})
+    }
+    handleVendorChange(e){
+        this.setState({vendor:e.target.value})
+    }
+    handleTagChange(e){
+        this.setState({tag:e.target.value})
+    }
+    handleAmountChange(e){
+        this.setState({amount:e.target.value})
+    }
+
+    postToApi(e){
+        e.preventDefault()
+        axios({
+            url: "/addTransaction",
+            method: "POST",
+            data:{
+                id: 2,
+                accountId: 25,
+                date:this.state.date,
+                 displayName:this.state.vendor,
+                  bucketTag:this.state.tag,
+                   amount:this.state.amount
+                }
+          })
+    }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                <form>
+        <h3>Date:</h3>
+        <input type="text" className="form-control" placeholder="Date" onChange={this.handleDateChange}></input>
         <h3>Vendor:</h3>
-        <input type="text" class="form-control" placeholder="Vendor"></input>
+        <input type="text" className="form-control" placeholder="Vendor" onChange={this.handleVendorChange}></input>
         <h3>Tag:</h3>
-            <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                Tag
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><a class="dropdown-item" href="#">Auto</a></li>
-                <li><a class="dropdown-item" href="#">Home</a></li>
-                <li><a class="dropdown-item" href="#">Food</a></li>
-                <li><a class="dropdown-item" href="#">Loans</a></li>
-                <li><a class="dropdown-item" href="#">Luxury</a></li>
-            </ul>
+            <div classNameName="dropdown">
+            <select onChange={this.handleTagChange}>
+                <option value="" disabled selected>--Select Tag--</option>
+                <option value="Auto">Auto</option>
+                <option value="Loans">Loans</option>
+                <option value="Food">Food</option>
+            </select>
         </div>
         <h3>Amount:</h3>
         
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text">$</span>
+        <div className="input-group mb-3">
+            <div className="input-group-prepend">
+                <span className="input-group-text">$</span>
             </div>
-            <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)"></input>
-        <div class="input-group-append">
-            <span class="input-group-text">.00</span>
+            <input type="text" onChange={this.handleAmountChange} className="form-control" aria-label="Amount (to the nearest dollar)"></input>
+        <div className="input-group-append">
+            <span className="input-group-text">.00</span>
         </div>
     </div>
     <center>
-        <button type="button" class="btn btn-success align-self-center" >Add Transaction</button>
+        <button type="button" className="btn btn-success align-self-center" onClick={this.postToApi}>Add Transaction</button>
     </center>
 
         </form>
-      </Container>
-    </div>
-  );
+      </Container> 
+            </div>
+        )
+        }
 }
