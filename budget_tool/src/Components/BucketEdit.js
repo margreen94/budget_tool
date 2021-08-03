90397568f66f42b579c80d9964cc9da4f6593146
John Won <johnjywon@gmail.com>
2021-08-03 11:11:48 -0400

    added new month and styled month

--- budget_tool/src/Components/BucketEdit.js
+++ budget_tool/src/Components/BucketEdit.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
+import "../App.css";
 
 import axios from "axios";
 
-import { Container } from "react-bootstrap";
+import {
+  Container,
+  Button,
+  InputGroup,
+  FormControl,
+  Row,
+} from "react-bootstrap";
 import EditCards from "./EditCards";
-
 
 export default class BucketEdit extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       bucket: [],
       editedIncome: 0,
@@ -16,11 +22,11 @@ export default class BucketEdit extends Component {
       totalBudget: "",
     };
 
-    this.addToEditedBuckets = this.addToEditedBuckets.bind(this)
-    this.editBucketsAPI = this.editBucketsAPI.bind(this)
-    this.incomeChange = this.incomeChange.bind(this)
+    this.addToEditedBuckets = this.addToEditedBuckets.bind(this);
+    this.editBucketsAPI = this.editBucketsAPI.bind(this);
+    this.incomeChange = this.incomeChange.bind(this);
   }
-  
+
   componentDidMount() {
     axios({
       url: "/getByAccountId/25",
@@ -37,28 +43,26 @@ export default class BucketEdit extends Component {
 
   addToEditedBuckets(bucket) {
     var newlist = this.state.editedBuckets;
-    var idList = []
-    for(var i in newlist) {
+    var idList = [];
+    for (var i in newlist) {
       idList.push(newlist[i].id);
     }
 
-    if(idList.includes(bucket.id) ){
-      for(var j in newlist) {
-        if(newlist[j].id === bucket.id) {
-          newlist[j].percent = bucket.percent
+    if (idList.includes(bucket.id)) {
+      for (var j in newlist) {
+        if (newlist[j].id === bucket.id) {
+          newlist[j].percent = bucket.percent;
         }
       }
-    }
-    else {
-      newlist.push(bucket)
+    } else {
+      newlist.push(bucket);
     }
 
-    this.setState({editeBuckets: newlist})
-
+    this.setState({ editeBuckets: newlist });
   }
 
   incomeChange(event) {
-    this.setState({editedIncome: event.target.value})
+    this.setState({ editedIncome: event.target.value });
   }
 
   editBucketsAPI() {
@@ -68,23 +72,54 @@ export default class BucketEdit extends Component {
       data: {
         income: this.state.editedIncome,
         buckets: this.state.editedBuckets,
-        accountId: 25
-      }}
-      );
-      window.location.reload(false);
+        accountId: 25,
+      },
+    });
+    window.location.reload(false);
   }
-
+  newSpendingMonth() {
+    axios({
+      url: "newSpendingMonth",
+      method: "POST",
+      data: { id: 25 },
+    });
+    window.location.reload(false);
+  }
   render() {
     return (
       <div>
-        <label>Estimated Montly Income: <input type="text" id="TotalAmount" onChange={this.incomeChange}></input></label>
         <Container>
-          <EditCards editData={this.state.bucket} addFunc={this.addToEditedBuckets}/>
-          <input
-                type="submit"
-                value="Submit"
-                onClick={this.editBucketsAPI}
-              ></input>
+          <Row className="justify-content-md-center spacing">
+            <InputGroup size="lg">
+              <InputGroup.Text id="inputGroup-sizing-lg">
+                Estimated Montly Income:{" "}
+              </InputGroup.Text>
+              <FormControl
+                aria-label="Large"
+                aria-describedby="inputGroup-sizing-sm"
+                type="text"
+                id="TotalAmount"
+                onChange={this.incomeChange}
+              />
+            </InputGroup>
+          </Row>
+        </Container>
+        <Container>
+          <EditCards
+            editData={this.state.bucket}
+            addFunc={this.addToEditedBuckets}
+          />
+          <Button
+            style={{ marginRight: "1em" }}
+            type="submit"
+            value="Submit"
+            onClick={this.editBucketsAPI}
+          >
+            Submit
+          </Button>
+          <Button type="submit" onClick={this.newSpendingMonth}>
+            New Month
+          </Button>
         </Container>
       </div>
     );
