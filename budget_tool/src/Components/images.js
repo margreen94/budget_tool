a5dc919e5afe1ea178cc82bd60698bd0abea0689
chloepinkston <chloe.pinkston@gmail.com>
2021-08-03 16:37:55 -0400

    added more images

--- budget_tool/src/Components/images.js
+++ budget_tool/src/Components/images.js
@@ -1,11 +1,24 @@
 import Car from "../images/Car.jpg";
-import Food from "../images/Food.jpg";
+import Food from "../images/food.jpg";
 import General from "../images/General.jpg";
 import Rent from "../images/Rent.jpg";
+//import Utilities from "../images/Utilities.jpg";
+//import Medical from "../images/Medical.jpg";
+//import PersonalCare from "../images/PersonalCare.jpg";
+//import Subscriptions from "../images/Subscriptions.jpg";
+//import Entertainment from "../images/Entertainment.jpg";
+//import Miscellaneous from "../images/Miscellaneous.jpg";
+
 
 export const images = {
   Car: Car,
   Food: Food,
   General: General,
-  Rent: Rent,
+  Rent: Rent
+ // Utilities: Utilities,
+  //Medical: Medical,
+ // PersonalCare: PersonalCare,
+  //Subscriptions: Subscriptions,
+  //Entertainment: Entertainment,
+ // Miscellaneous: Miscellaneous
 };
