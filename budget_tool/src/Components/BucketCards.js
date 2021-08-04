a18e6610cfb7a23d6fedcf609bf95043296cb983
briggsy007 <hunter.briggs@outlook.com>
2021-08-04 14:13:09 -0400

    Fixed Overflow of Progress bar

--- budget_tool/src/Components/BucketCards.js
+++ budget_tool/src/Components/BucketCards.js
@@ -4,13 +4,26 @@ import "../App.css";
 import { Row } from "react-bootstrap";
 import { Card, ListGroupItem, ListGroup, Col } from "react-bootstrap";
 import { images } from "./images";
-let percentage
+
 export default class BucketCards extends Component {
   render() {
     const cards = [];
     for (var i in this.props.cardData) {
-      percentage = (this.props.cardData[i].amountSpent/this.props.cardData[i].amountGoal) * 100;
+      let percentage
+      let setColor = "green"
       
+      percentage = (this.props.cardData[i].amountSpent/this.props.cardData[i].amountGoal) * 100;
+      let widthPercent = percentage
+      if(Number.isNaN(percentage)){
+        percentage = 0.0
+        widthPercent = 0.0
+        console.log(widthPercent)
+      }
+      if(percentage>100.00){
+        setColor = "red"
+        widthPercent = 100.00
+      }
+
       cards.push(
         <Col md={4} key={i} className="spacing">
           <Card>
@@ -29,7 +42,7 @@ export default class BucketCards extends Component {
                 Budget Goal: {this.props.cardData[i].amountGoal}
               </ListGroupItem>
               <ListGroupItem>
-                <div className="progress-bar" role="progressbar" id="bar"  style={{ width: ''+percentage+'%'}} >
+                <div className="progress-bar" role="progressbar" id="bar"  style={{ width: ''+widthPercent+'%' , backgroundColor: setColor}}>
                   {percentage.toFixed(2)}% of Budget Spent
                 </div>
               </ListGroupItem>
